b7da0572ce15be07853e5f95809809ca15e18347
Akash <proakash256@gmail.com>
2024-03-27 16:00:31 +0530

    Partitioning the data

---
+++ model_building/dataset_partition.py
@@ -0,0 +1,13 @@
+def get_dataset_partitions(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    ds_size = len(ds)
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=123)
+
+        train_size = int(train_split * ds_size)
+        val_size = int(val_split * ds_size)
+
+        train_ds = ds.take(train_size)
+        val_ds = ds.skip(train_size).take(val_size)
+        test_ds = ds.skip(train_size).skip(val_size)
+
+    return train_ds, val_ds, test_ds
